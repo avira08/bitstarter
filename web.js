74180379527a91b7ffd9334e69af018d4ea6073c
avira_08 <aditi.vira@gmail.com>
2013-07-12 05:34:45 +0000

    changed web.js to reflect changed msg

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs=require('fs');
+var data=fs.readFileSync('index.html');
+
+var buf=new Buffer(data,'utf-8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
